3517147a5c7c990e6dbdb3a30a9d0bb4969516b0
Anwar Abuukar <anwarabuukar6@gmail.com>
2026-02-01 10:09:18 +0300

    setup project directory

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+
+export  const mongoconfig = process.env.MONGODB_CONNECTION_STRING
+export const serverport = process.env.PORT 
+export const jwtsectret = process.env.JWT_SECRET
+export const jwtaudience = process.env.JWT_AUDIENCE
+export const jwtissuer = process.env.JWT_ISSUER
